15ba38268aa0f67eed84406cfecbb580cb14f9d2
Adarsh Alone <anitaalone1974@gmail.com>
2022-08-21 12:02:55 +0530

    my commit

---
+++ practice.js
@@ -0,0 +1,35 @@
+console.log("hello world");
+// var a = 5;
+// var a = 6
+// let b = 59;
+//  b = 55;
+
+// const c = 57;
+//  d = 65;
+
+// console.log(a);
+// console.log(b);
+// console.log(d);
+
+// {
+//     const c = 50
+//     console.log(c)
+// }
+
+// const myVar = function (a, b) {
+//     return a + b;
+// }
+
+// console.log(myVar(2, 3));
+
+// function mySum() {
+//     const e = 50;
+//     const b = 50;
+//     return e + b;
+// }
+// const action = mySum();
+// console.log(action);
+
+let arr = ["Akash", "Lovish","Kutub","Sakshi",{love:"hate",
+baby:"Shower",
+alan:"walker"},"12love","hate58"];
